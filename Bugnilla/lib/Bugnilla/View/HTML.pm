9f10b1214c048abb6d8e5782185854a84d29e5b8
Chris Fulton <828498+angwonson@users.noreply.github.com>
2021-11-18 17:35:26 -0800

    JIRA-1 added wrapper template

--- Bugnilla/lib/Bugnilla/View/HTML.pm
+++ Bugnilla/lib/Bugnilla/View/HTML.pm
@@ -6,7 +6,15 @@ extends 'Catalyst::View::TT';
 
 __PACKAGE__->config(
     TEMPLATE_EXTENSION => '.tt2',
-    render_die => 1,
+
+    # Set the location for TT files
+    INCLUDE_PATH => [
+            Bugnilla->path_to( 'root', 'src' ),
+        ],
+    # Set to 1 for detailed timer stats in your HTML as comments
+    TIMER              => 0,
+    # This is your wrapper template located in the 'root/src'
+    WRAPPER => 'wrapper.tt2',
 );
 
 =head1 NAME

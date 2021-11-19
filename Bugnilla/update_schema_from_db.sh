3546dc0eb4abad2a08cbbc7bdfe9e4a95026b872
Chris Fulton <828498+angwonson@users.noreply.github.com>
2021-11-19 15:59:16 -0800

    JIRA-1 starting conversion to sha1 for password storage

--- Bugnilla/update_schema_from_db.sh
+++ Bugnilla/update_schema_from_db.sh
@@ -2,5 +2,5 @@
 # overwrite_modifications=1
 
 script/bugnilla_create.pl model DB DBIC::Schema Bugnilla::Schema \
-    create=static components=TimeStamp naming-current use_namespaces=1 dbi:SQLite:bugnilla.db \
+    create=static components=TimeStamp,PassphraseColumn naming-current use_namespaces=1 dbi:SQLite:bugnilla.db \
     on_connect_do="PRAGMA foreign_keys = ON"

523aeda42855ff62ed333079a574fe05dfc9971f
Chris Fulton <828498+angwonson@users.noreply.github.com>
2021-11-19 15:11:02 -0800

    JIRA-1 adding users schema and Classes

--- Bugnilla/init_sqlite_db.sh
+++ Bugnilla/init_sqlite_db.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sqlite3 bugnilla.db < schema/tickets.sql
+sqlite3 bugnilla.db < schema/users.sql
---
+++ Bugnilla/lib/Bugnilla/Schema/Result/User.pm
@@ -0,0 +1,144 @@
+use utf8;
+package Bugnilla::Schema::Result::User;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Bugnilla::Schema::Result::User
+
+=cut
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use MooseX::MarkAsMethods autoclean => 1;
+extends 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=item * L<DBIx::Class::TimeStamp>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp");
+
+=head1 TABLE: C<users>
+
+=cut
+
+__PACKAGE__->table("users");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 username
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 password
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 email_address
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 first_name
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 last_name
+
+  data_type: 'text'
+  is_nullable: 1
+
+=head2 active
+
+  data_type: 'integer'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "username",
+  { data_type => "text", is_nullable => 1 },
+  "password",
+  { data_type => "text", is_nullable => 1 },
+  "email_address",
+  { data_type => "text", is_nullable => 1 },
+  "first_name",
+  { data_type => "text", is_nullable => 1 },
+  "last_name",
+  { data_type => "text", is_nullable => 1 },
+  "active",
+  { data_type => "integer", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 user_roles
+
+Type: has_many
+
+Related object: L<Bugnilla::Schema::Result::UserRole>
+
+=cut
+
+__PACKAGE__->has_many(
+  "user_roles",
+  "Bugnilla::Schema::Result::UserRole",
+  { "foreign.user_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 roles
+
+Type: many_to_many
+
+Composing rels: L</user_roles> -> role
+
+=cut
+
+__PACKAGE__->many_to_many("roles", "user_roles", "role");
+
+
+# Created by DBIx::Class::Schema::Loader v0.07049 @ 2021-11-19 15:09:52
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:bWNznuTdLTgsjMLulzIFcw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;

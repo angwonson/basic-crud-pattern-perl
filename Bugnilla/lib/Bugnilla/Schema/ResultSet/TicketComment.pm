4873ab87ee1db4534b4c2dee36ffc7fd93482e98
Chris Fulton <828498+angwonson@users.noreply.github.com>
2021-11-22 01:08:52 -0800

    JIRA-1 comments seem to be working ok

--- Bugnilla/lib/Bugnilla/Schema/ResultSet/TicketComment.pm
+++ Bugnilla/lib/Bugnilla/Schema/ResultSet/TicketComment.pm
@@ -10,11 +10,14 @@ A predefined search for ticket comments
 
 =cut
 
+# reverse the order, newest to oldest
+# order_by => { -desc => qw/col1 col2 col3/}
+# order_by => { -asc => 'col'}
 sub get_comments_by_ticket_id {
     my ($self, $ticket_id) = @_;
     return $self->search({
         ticket_id => $ticket_id
-    });
+    }, {order_by => { -desc => 'id'}});
 }
 
 1;
